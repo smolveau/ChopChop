9b49f8e1231a76899ec2de6f72c155c742a86f59
PulsZ <neostan49@gmail.com>
2020-10-25 12:05:24 +0100

    Fix error colors declared outside function

--- userside/formatting/format_output_table.go
+++ userside/formatting/format_output_table.go
@@ -1,21 +1,20 @@
 package formatting
 
 import (
+	"fmt"
 	"gochopchop/core"
 	"os"
 
 	"github.com/jedib0t/go-pretty/table"
 )
 
-colorReset := "\033[0m"
-colorRed := "\033[31m"
-colorGreen := "\033[32m"
-colorYellow := "\033[33m"
-colorCyan := "\033[36m"
-
 // PrintTable will render the data as a nice table
 func PrintTable(out []core.Output) {
-
+	colorReset := "\033[0m"
+	colorRed := "\033[31m"
+	colorGreen := "\033[32m"
+	colorYellow := "\033[33m"
+	colorCyan := "\033[36m"
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.AppendHeader(table.Row{"URL", "Endpoint", "Severity", "Plugin", "Remediation"})
@@ -29,7 +28,7 @@ func PrintTable(out []core.Output) {
 			severity = fmt.Sprint(string(colorGreen), "Low", string(colorReset))
 		} else {
 			severity = fmt.Sprint(string(colorCyan), "Informational", string(colorReset))
-		} 
+		}
 		t.AppendRow([]interface{}{
 			output.URL,
 			output.Endpoint,
