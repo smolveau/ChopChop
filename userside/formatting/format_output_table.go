241e748a5b6e0a14f8cbbc685ab1d24bd7b46ab4
PulsZ <neostan49@gmail.com>
2020-10-25 11:52:23 +0100

    Adding colors for table output, sorted by severity

--- userside/formatting/format_output_table.go
+++ userside/formatting/format_output_table.go
@@ -7,6 +7,12 @@ import (
 	"github.com/jedib0t/go-pretty/table"
 )
 
+colorReset := "\033[0m"
+colorRed := "\033[31m"
+colorGreen := "\033[32m"
+colorYellow := "\033[33m"
+colorCyan := "\033[36m"
+
 // PrintTable will render the data as a nice table
 func PrintTable(out []core.Output) {
 
@@ -14,10 +20,20 @@ func PrintTable(out []core.Output) {
 	t.SetOutputMirror(os.Stdout)
 	t.AppendHeader(table.Row{"URL", "Endpoint", "Severity", "Plugin", "Remediation"})
 	for _, output := range out {
+		severity := ""
+		if out[i].Severity == "High" {
+			severity = fmt.Sprint(string(colorRed), "High", string(colorReset))
+		} else if out[i].Severity == "Medium" {
+			severity = fmt.Sprint(string(colorYellow), "Medium", string(colorReset))
+		} else if out[i].Severity == "Low" {
+			severity = fmt.Sprint(string(colorGreen), "Low", string(colorReset))
+		} else {
+			severity = fmt.Sprint(string(colorCyan), "Informational", string(colorReset))
+		} 
 		t.AppendRow([]interface{}{
 			output.URL,
 			output.Endpoint,
-			output.Severity,
+			severity,
 			output.PluginName,
 			output.Remediation,
 		})
